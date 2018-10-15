42315ad0b986709778b6b6397a1bbfe075348f2b
TheVigor <igor.proskuryakov14@gmail.com>
2018-10-16 00:33:53 +0300

    started implementing navigation ui

--- app/src/main/java/com/noble/activity/dembeliscoming/MainActivity.kt
+++ app/src/main/java/com/noble/activity/dembeliscoming/MainActivity.kt
@@ -2,11 +2,18 @@ package com.noble.activity.dembeliscoming
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(this, R.id.nav_host_fragment)
     }
 }
---
+++ app/src/main/java/com/noble/activity/dembeliscoming/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.noble.activity.dembeliscoming
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.*
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import kotlinx.android.synthetic.main.main_fragment.*
+
+/**
+ * Fragment used to show how to navigate to another destination
+ */
+class MainFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val options = NavOptions.Builder()
+            .setEnterAnim(R.anim.slide_in_right)
+            .setExitAnim(R.anim.slide_out_left)
+            .setPopEnterAnim(R.anim.slide_in_left)
+            .setPopExitAnim(R.anim.slide_out_right)
+            .build()
+
+        navigate_dest_bt.setOnClickListener {
+            findNavController(it).navigate(R.id.settingsFragment, null, options)
+        }
+
+        navigate_action_bt.setOnClickListener {
+            findNavController(it).navigate(R.id.action_mainFragment_to_settingsFragment2)
+        }
+    }
+
+}
