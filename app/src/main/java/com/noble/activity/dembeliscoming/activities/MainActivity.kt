51023f08e7fda892498985d0070d6fe869f13bff
TheVigor <igor.proskuryakov14@gmail.com>
2018-10-18 00:51:30 +0300

    added navigation drawer with action bar using data bindings

--- app/src/main/java/com/noble/activity/dembeliscoming/activities/MainActivity.kt
+++ app/src/main/java/com/noble/activity/dembeliscoming/activities/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.noble.activity.dembeliscoming.activities
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.noble.activity.dembeliscoming.R
+import com.noble.activity.dembeliscoming.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        // Set up ActionBar
+        setSupportActionBar(binding.toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        // Set up navigation menu
+        binding.navigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(drawerLayout,
+            Navigation.findNavController(this, R.id.nav_host_fragment))
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/noble/activity/dembeliscoming/fragments/SplashFragment.kt
+++ app/src/main/java/com/noble/activity/dembeliscoming/fragments/SplashFragment.kt
@@ -7,26 +7,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
-
 import com.noble.activity.dembeliscoming.R
 import kotlinx.android.synthetic.main.fragment_splash.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SplashFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class SplashFragment: Fragment() {
 
     override fun onCreateView(
@@ -39,6 +24,7 @@ class SplashFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         next_btn.setOnClickListener {
             findNavController(it).navigate(R.id.splashToLogin)
