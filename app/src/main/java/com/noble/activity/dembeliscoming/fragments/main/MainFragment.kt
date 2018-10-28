5c3d70cddcc3485f0aabea0aefcba9d2064bfb75
TheVigor <igor.proskuryakov14@gmail.com>
2018-10-28 17:57:49 +0300

    added Timer for update dembel time status every second

--- app/src/main/java/com/noble/activity/dembeliscoming/fragments/main/MainFragment.kt
+++ app/src/main/java/com/noble/activity/dembeliscoming/fragments/main/MainFragment.kt
@@ -1,17 +1,27 @@
 package com.noble.activity.dembeliscoming.fragments.main
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.view.*
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation.findNavController
 import com.noble.activity.dembeliscoming.R
+import com.noble.activity.dembeliscoming.showToast
 import kotlinx.android.synthetic.main.main_fragment.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Fragment used to show how to navigate to another destination
  */
 class MainFragment : Fragment() {
+
+    private val timer = Timer("time_to_dembel", true)
+
+    private val updateInerval: Long = 1000
+    private val updateDelay: Long = 0
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         setHasOptionsMenu(true)
@@ -35,6 +45,17 @@ class MainFragment : Fragment() {
         navigate_action_bt.setOnClickListener {
             findNavController(it).navigate(R.id.action_mainFragment_to_settingsFragment2)
         }
-    }
 
+        timer.scheduleAtFixedRate(object: TimerTask(){
+            override fun run() {
+                activity?.runOnUiThread {
+                    val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+                    val currentDateandTime = sdf.format(Date())
+                    text?.text = currentDateandTime
+                }
+            }
+        }, updateDelay, updateInerval)
+
+    }
+    
 }
