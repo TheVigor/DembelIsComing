8cc9b5facd326356e14598be606cb5558e94be44
TheVigor <igor.proskuryakov14@gmail.com>
2018-11-05 12:05:42 +0300

    added constants

---
+++ app/src/main/java/com/noble/activity/dembeliscoming/utilities/Constants.kt
@@ -0,0 +1,7 @@
+package com.noble.activity.dembeliscoming.utilities
+
+/**
+ * Constants used throughout the app.
+ */
+const val DATABASE_NAME = "dmb-db"
+const val DRILL_DOCS_FILENAME = "drill_docs.json"
