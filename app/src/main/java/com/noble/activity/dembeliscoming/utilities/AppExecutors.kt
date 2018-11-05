768c2a569e5fed6a27038d7e00c918b0b27b6753
TheVigor <igor.proskuryakov14@gmail.com>
2018-11-05 12:03:29 +0300

    added app executor for background work

---
+++ app/src/main/java/com/noble/activity/dembeliscoming/utilities/AppExecutors.kt
@@ -0,0 +1,9 @@
+package com.noble.activity.dembeliscoming.utilities
+
+import java.util.concurrent.Executors
+
+private val IO_EXECUTOR = Executors.newSingleThreadExecutor()
+
+fun runOnIoThread(f: () -> Unit) {
+    IO_EXECUTOR.execute(f)
+}
