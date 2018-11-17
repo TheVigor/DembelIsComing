f2d730185d2d52e364017dc66dfc1203994601f1
TheVigor <igor.proskuryakov14@gmail.com>
2018-11-17 18:09:58 +0300

    refactoring -> now render UI state based on enum state

--- app/src/main/java/com/noble/activity/dembeliscoming/fragments/main/MainFragment.kt
+++ app/src/main/java/com/noble/activity/dembeliscoming/fragments/main/MainFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.*
 import com.noble.activity.dembeliscoming.*
+import com.noble.activity.dembeliscoming.fragments.main.DembelState.DEFAULT
+import com.noble.activity.dembeliscoming.fragments.main.DembelState.NOT_STARTED
+import com.noble.activity.dembeliscoming.fragments.main.DembelState.IN_PROGRESS
+import com.noble.activity.dembeliscoming.fragments.main.DembelState.FINISHED
 import kotlinx.android.synthetic.main.main_fragment.*
 import kotlinx.android.synthetic.main.timer_main.view.*
 import java.util.*
@@ -11,16 +15,20 @@ import com.noble.activity.dembeliscoming.utilities.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nav_header.view.*
 
-/**
- * Fragment used to show how to navigate to another destination
- */
+enum class DembelState {
+    DEFAULT,
+    NOT_STARTED,
+    IN_PROGRESS,
+    FINISHED
+}
+
 class MainFragment : Fragment() {
 
     private val timer = Timer("time_to_dembel", true)
 
     private val updateInerval: Long = 1000
     private val updateDelay: Long = 0
-
+    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         setHasOptionsMenu(true)
@@ -29,7 +37,44 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
+        renderDefaultState()
+
+        timer.scheduleAtFixedRate(object: TimerTask(){
+            override fun run() {
+                val currentDate = Date().time
+                val currentState = getStateByCurrentDate(currentDate)
+
+                activity?.runOnUiThread {
+                    updateDembelState(currentState, currentDate)
+                }
+            }
+        }, updateDelay, updateInerval)
+
+    }
+
+    private fun getStateByCurrentDate(currentDate: Long): DembelState {
+        if (currentDate < soldierPrefs.startDate) {
+            return NOT_STARTED
+        }
+
+        if (currentDate >= soldierPrefs.endDate) {
+            return FINISHED
+        }
+
+        return IN_PROGRESS
+    }
+
+    private fun updateDembelState(state: DembelState, currentDate: Long) =
+        when (state) {
+            DEFAULT -> renderDefaultState()
+            NOT_STARTED -> renderNotStartedState(currentDate)
+            IN_PROGRESS -> renderInProgressState(currentDate)
+            FINISHED -> renderFinishedState()
+        }
+
+
+    private fun renderDefaultState() {
         activity?.navigation_view?.getHeaderView(0)?.header_name?.text = soldierPrefs.soldierName
 
         chart?.init()
@@ -45,70 +90,53 @@ class MainFragment : Fragment() {
 
         leftDembelTimer?.title_text?.text = "Left"
         leftDembelTimer?.timer_icon?.setImageResource(R.drawable.sad_soldier)
+    }
 
-        timer.scheduleAtFixedRate(object: TimerTask(){
-            override fun run() {
-                activity?.runOnUiThread {
-                    val currentDate = Date().time
+    private fun renderNotStartedState(currentDate: Long) {
+        notStartedTimer?.visibility = View.VISIBLE
 
-                    if (currentDate < soldierPrefs.startDate) {
+        passedDembelTimer?.visibility = View.GONE
+        leftDembelTimer?.visibility = View.GONE
+        chart?.visibility = View.GONE
+        finishedText?.visibility = View.GONE
 
-                        notStartedTimer?.visibility = View.VISIBLE
+        val diffTimeToStart = getDiffTime(currentDate, soldierPrefs.startDate)
 
-                        passedDembelTimer?.visibility = View.GONE
-                        leftDembelTimer?.visibility = View.GONE
-                        chart?.visibility = View.GONE
-                        finishedText?.visibility = View.GONE
+        notStartedTimer?.updateCounter(diffTimeToStart)
+        notStartedTimer?.percentage_text?.text = "${mkTxtDateByLong(soldierPrefs.startDate)}"
+    }
 
-                        val diffTimeToStart =
-                            getDiffTime(currentDate, soldierPrefs.startDate)
+    private fun renderInProgressState(currentDate: Long) {
+        notStartedTimer?.visibility = View.GONE
+        finishedText?.visibility = View.GONE
 
-                        notStartedTimer?.updateCounter(diffTimeToStart)
-                        notStartedTimer?.percentage_text?.text =
-                                "Starting date: ${mkTxtDateByLong(soldierPrefs.startDate)}"
+        passedDembelTimer?.visibility = View.VISIBLE
+        leftDembelTimer?.visibility = View.VISIBLE
+        chart?.visibility = View.VISIBLE
 
-                        return@runOnUiThread
-                    }
+        val diffTimeFromStart =
+            getDiffTime(soldierPrefs.startDate, currentDate)
+        val diffTimeToDembel =
+            getDiffTime(currentDate, soldierPrefs.endDate)
 
-                    if (currentDate >= soldierPrefs.endDate) {
+        passedDembelTimer?.updateCounter(diffTimeFromStart)
+        passedDembelTimer?.updatePassedPercentage(currentDate)
 
-                        finishedText?.visibility = View.VISIBLE
+        leftDembelTimer?.updateCounter(diffTimeToDembel)
+        leftDembelTimer?.updateLeftPercentage(currentDate)
 
-                        passedDembelTimer?.visibility = View.GONE
-                        leftDembelTimer?.visibility = View.GONE
-                        chart?.visibility = View.GONE
-                        notStartedTimer?.visibility = View.GONE
+        chart?.updateData(
+            percentagePassed(currentDate).replace(',', '.').toFloat(),
+            percentageLeft(currentDate).replace(',', '.').toFloat())
+    }
 
-                        return@runOnUiThread
-                    }
+    private fun renderFinishedState() {
+        finishedText?.visibility = View.VISIBLE
 
-                    notStartedTimer?.visibility = View.GONE
-                    finishedText?.visibility = View.GONE
-
-                    passedDembelTimer?.visibility = View.VISIBLE
-                    leftDembelTimer?.visibility = View.VISIBLE
-                    chart?.visibility = View.VISIBLE
-
-
-                    val diffTimeFromStart =
-                        getDiffTime(soldierPrefs.startDate, currentDate)
-                    val diffTimeToDembel =
-                        getDiffTime(currentDate, soldierPrefs.endDate)
-
-                    passedDembelTimer?.updateCounter(diffTimeFromStart)
-                    passedDembelTimer?.updatePassedPercentage(currentDate)
-
-                    leftDembelTimer?.updateCounter(diffTimeToDembel)
-                    leftDembelTimer?.updateLeftPercentage(currentDate)
-
-                    chart?.updateData(
-                        percentagePassed(currentDate).replace(',', '.').toFloat(),
-                        percentageLeft(currentDate).replace(',', '.').toFloat())
-
-                }
-            }
-        }, updateDelay, updateInerval)
-
+        passedDembelTimer?.visibility = View.GONE
+        leftDembelTimer?.visibility = View.GONE
+        chart?.visibility = View.GONE
+        notStartedTimer?.visibility = View.GONE
     }
 
 }
